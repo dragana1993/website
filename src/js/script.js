0b3a14fe4c6cceff7f4edd0d809b0b109ba40fde
dragana1993 <80923238+dragana1993@users.noreply.github.com>
2021-03-20 20:40:14 +0100

    Veliki update za sajt
    -Nova stranica
        -Login popup
    -Sredjen kod
    -Rasporedjeni fajlovi

--- src/js/script.js
+++ src/js/script.js
@@ -16,3 +16,13 @@ function hideDropbox(e) {
     var dropbox = document.getElementById("riodrop");
     dropbox.style.display = "none";
 }
+
+function showLogin() {
+    var login_container = document.getElementsByClassName("login-container")[0];
+    login_container.style.visibility = "visible";
+}
+
+function hideLogin() {
+    var login_container = document.getElementsByClassName("login-container")[0];
+    login_container.style.visibility = "hidden";
+}
